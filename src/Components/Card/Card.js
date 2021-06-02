7a461c9aee0eb28fea6ddcb74c5c9cc213afad81
Vitalij <almizvitalij@gmail.com>
2021-06-02 21:21:17 +0300

    components structure

---
+++ src/Components/Card/Card.js
@@ -0,0 +1,10 @@
+// получает объект со свойствами и рендерит карточку
+// {
+//     type: 'task / challenge',
+//     difficulty: 'light / medium / hard',
+//     categorie: '',
+//     date: '',
+//     text: '',
+// }
+//
+// по клику на карточку рендерит CreateEditCard
--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -1,3 +1,6 @@
 export default function main() {
   return <h1>Main page</h1>;
 }
+// Рендерит хедер, два списка карточек (Today, Tomorrow - компонент CardList) кнопку Done
+// При монтировании феттчит все НЕ выполненные карточки и кладет в CardList-ы (Today, Tomorrow)
+// По клику на кнопку Done фетчится список выполненных карточек и рендерится третий CardList
