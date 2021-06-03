b81b7a1581b88e38e7f4dc531806502cf1b85c18
7Romuch7 <64313145+7Romuch7@users.noreply.github.com>
2021-06-04 02:15:00 +0300

    Landing greate

---
+++ src/Pages/AuthPage/AuthPage.js
@@ -0,0 +1,48 @@
+import styles from '../AuthPage/AuthPage.module.css';
+
+export default function Landing() {
+
+    return (
+        <div className={styles.containerLanding}>
+            <div className={styles.containerQuest}>
+                <div className={styles.containerForm}>
+                    <h1 className={styles.logo}>Questify</h1>
+                        <p className={styles.description}>
+                            Questify will turn your life into a thrilling
+                            game full of amazing quests
+                            and exciting challenges.
+                        </p>
+                        <p className={styles.descriptionRegister}>Write your email to sign up or log in</p>
+                
+                        <form className={styles.form} /* onSubmit={handleSubmit} */>
+            
+                            <input className={styles.inputForm}
+                                type="text"
+                                name="email"
+                                //value={email}
+                                placeholder="Email"
+                                /* id={emailInputId}
+                                onChange={handleChange} */
+                            />
+                    
+                            <input className={styles.inputForm}
+                                type="number"
+                                name="password"
+                                /* value={number} */
+                                placeholder="Password"
+                                /* id={passwordInputId}
+                                onChange={handleChange} */
+                            />
+
+                            <button
+                                /* disabled={!email && !password < 1} */
+                                className={styles.btnForm}
+                                type="submit">
+                                go!
+                            </button>
+                        </form>
+                </div>
+            </div>
+        </div>
+    )
+}
