60e19fc9318a9eb6f4787ed6c9b6c100af778dc0
Vitalij <almizvitalij@gmail.com>
2021-06-03 11:11:56 +0300

    set common styles

--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -1,5 +1,19 @@
-export default function main() {
-  return <h1>Main page</h1>;
+import CardList from '../../Components/CardList/CardList';
+import Icon from '../../Components/Icon';
+import s from './MainPage.module.css';
+
+export default function Main() {
+  return (
+    <div>
+      {/* <CardList cards={}/> */}
+      {/* <CardList cards={}/> */}
+      <button className={s.btn}>
+        DONE
+        <Icon name={'triangle-down'} size={12} />
+      </button>
+      {/* <CardList cards={}/> */}
+    </div>
+  );
 }
 // Рендерит хедер, два списка карточек (Today, Tomorrow - компонент CardList) кнопку Done
 // При монтировании феттчит все НЕ выполненные карточки и кладет в CardList-ы (Today, Tomorrow)
