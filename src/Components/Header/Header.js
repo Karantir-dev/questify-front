cee0d9e45f1e164b9252dfd54306c538b9250765
Tetiana Bronitska <tanya.nest87@gmail.com>
2021-06-04 12:14:05 +0300

    Added Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,16 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
 import s from './Header.module.css'
 import defaultAvatar from './codecode.jpg'
-import IconButton from './IconButton'
 import Icon from '../../Components/Icon'
 // import authSelectors from '../../Redux/auth/auth-selectors'
 // import authOperations from '../../Redux/auth/auth-operations'
 
 export default function Header() {
     // const email = useSelector(authSelectors.getUserEmail);
+    // const name = useSelector(authSelectors.getUserName);
     // const emailParts = email.split('@')
-    // const name = emailParts[0]
+    // const nameFromEmail = emailParts[0]
 
     //   const onLogOut = useCallback(() => {
     // dispatch(authOperations.logOut());
@@ -20,17 +22,23 @@ export default function Header() {
             <div className={s.Logo}>
                 Questify
             </div>
-            <div className={s.MenuImgName}>
+            <div className={s.MenuImgNameLogout}>
                 <img className={s.MenuImg} src={defaultAvatar} alt="avatar" width="30" />
                 <span className={s.MenuName}>name's Quest Log</span>
+                {/* change name to {name==='Guest' ? nameFromEmail : name} */}
             </div>
-            <IconButton
-                // onClick={onLogout}
-                aria-label="Logout">
-                
+             <button type="button"
+                // onClick={onLogOut}
+                     className={s.IconButton}
+                     aria-label="Logout">
                 <Icon name={'logout'} size={22}/>
-
-            </IconButton>
+                </button>
         </header>
     )
 }
+
+
+// Header.propTypes = {
+//     onClick: PropTypes.func,
+//     'aria-label': PropTypes.string.isRequired,
+// }
