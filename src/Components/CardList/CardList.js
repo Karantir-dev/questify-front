93921dca914dac417ec5f23d77ac8949d3cb3bd3
Viktoriia Strelnikova <56576729+mobijam@users.noreply.github.com>
2021-06-04 14:39:01 +0300

    add card list component without style module

--- src/Components/CardList/CardList.js
+++ src/Components/CardList/CardList.js
@@ -1,1 +1,35 @@
-// получает список карточек и рендерит компоненты Card
+import React from 'react';
+import PropTypes from 'prop-types';
+import Card from '../Card/Card';
+import './CardList.module.css';
+
+function CardList({cards}) {
+    return (
+      <ul>
+        {cards.map(({ type, difficulty, categorie, date, text }) => (
+            <Card
+            type={type}
+            difficulty={difficulty}
+            categorie={categorie}
+            date={date}
+            text={text}
+            /> 
+        ))}
+      </ul>
+    );
+  };
+
+CardList.propTypes = {
+    contacts: PropTypes.arrayOf(
+      PropTypes.shape({
+        type: PropTypes.string.isRequired,
+        difficulty: PropTypes.string.isRequired,
+        categorie: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired,
+        text: PropTypes.string.isRequired,
+      }),
+    ),
+
+  };
+  
+  export default CardList;
