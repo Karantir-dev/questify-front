423b65afa29d2f2705894c5ecfdd7bb94e03de61
Alina Bovdyr <alinabovdyr92@gmail.com>
2021-06-05 12:00:57 +0300

    Add cards Actions, Operations, Reducers, Selectors. Change store for cards

--- src/Redux/cards/cardsOperations.js
+++ src/Redux/cards/cardsOperations.js
@@ -6,25 +6,25 @@ axios.defaults.baseURL = 'https://goit23-project.herokuapp.com/'
 const fetchActiveCards = () => dispatch => {
   dispatch(cardsActions.fetchActiveCardsRequest())
 
-  // axios
-  //   .get('cards/?isComplited=false')
-  //   .then(({ data }) => {
-  //     dispatch(cardsActions.fetchActiveCardsSuccess(data));
-  //   })
-  //   .catch(err =>
-  //     dispatch(
-  //       cardsActions.fetchActiveCardsError(
-  //         err.response?.data?.message || err.message,
-  //       ),
-  //     ),
-  //   );
+  axios
+    .get('/cards?isComplited=false')
+    .then(({ data }) => {
+      dispatch(cardsActions.fetchActiveCardsSuccess(data));
+    })
+    .catch(err =>
+      dispatch(
+        cardsActions.fetchActiveCardsError(
+          err.response?.data?.message || err.message,
+        ),
+      ),
+    );
 }
 
 const fetchDoneCards = () => dispatch => {
   dispatch(cardsActions.fetchDoneCardsRequest())
 
   axios
-    .get('cards/?isComplited=true')
+    .get('/cards?isComplited=true')
     .then(({ data }) => {
       dispatch(cardsActions.fetchDoneCardsSuccess(data))
     })
@@ -37,14 +37,91 @@ const fetchDoneCards = () => dispatch => {
     )
 }
 
+const addCard = (text, deadline, difficulty, category, isChallenge) => dispatch => {
+  const card = {
+    text,
+    deadline,
+    difficulty,
+    category,
+    isChallenge,
+    isCompleted: false,
+  }
+
+  dispatch(cardsActions.addCardRequest())
+
+  axios
+    .post('/cards', card)
+    .then(({ data }) => dispatch(cardsActions.addCardSuccess(data)))
+    .catch(err => dispatch(
+      cardsActions.addCardError(err.response?.data?.message || err.message)
+    ));
+};
+
+const editCard = (cardId, text, deadline, difficulty, category, isChallenge, isCompleted) => dispatch => {
+  const card = {
+    text,
+    deadline,
+    difficulty,
+    category,
+    isChallenge,
+    isCompleted,
+  }
+
+  dispatch(cardsActions.editCardRequest())
+
+  axios
+    .put(`/cards/${cardId}`, card)
+    .then(({data}) => dispatch(cardsActions.editCardSuccess(data)))
+    .catch(err => dispatch(
+      cardsActions.editCardError(err.response?.data?.message || err.message)
+    ));
+};
+
 const deleteCard = cardId => dispatch => {
   dispatch(cardsActions.deleteCardRequest())
 
   axios
     .delete(`/cards/${cardId}`)
     .then(() => dispatch(cardsActions.deleteCardSuccess(cardId)))
-    .catch(error => dispatch(cardsActions.deleteCardError(error.message)))
+    .catch(err => dispatch(
+      cardsActions.deleteCardError(err.response?.data?.message || err.message)
+    ))
 }
 
-const cardsOperations = { fetchActiveCards, fetchDoneCards, deleteCard }
+const toggleCompleted = ({ id, isCompleted }) => dispatch => {
+  const update = { isCompleted };
+
+  dispatch(cardsActions.toggleCompletedRequest());
+
+  axios
+    .patch(`/cards/${id}/complete`, update)
+    .then(({ data }) => dispatch(cardsActions.toggleCompletedSuccess(data)))
+    .catch(err => dispatch(
+      cardsActions.toggleCompletedError(err.response?.data?.message || err.message)
+    ));
+};
+
+const toggleChallenge = ({ id, isChallenge }) => dispatch => {
+  const update = { isChallenge };
+
+  dispatch(cardsActions.toggleChallengeRequest());
+
+  axios
+    .patch(`/cards/${id}/challenge`, update)
+    .then(({ data }) => dispatch(cardsActions.toggleChallengeSuccess(data)))
+    .catch(err => dispatch(
+      cardsActions.toggleChallengeError(err.response?.data?.message || err.message)
+    ));
+};
+
+
+const cardsOperations = {
+  fetchActiveCards,
+  fetchDoneCards,
+  addCard,
+  editCard,
+  deleteCard,
+  toggleCompleted,
+  toggleChallenge
+}
 export default cardsOperations
--- src/Redux/cards/cardsSelectors.js
+++ src/Redux/cards/cardsSelectors.js
@@ -1,16 +1,24 @@
 const getActiveTodayCards = state =>
-  state.activeCards.filter(
+  state.cards.activeCards.filter(
     card => new Date().toLocaleDateString() === card.date,
   )
+
 const getActiveTomorrowCards = state =>
-  state.activeCards.filter(
+  state.cards.activeCards.filter(
     card => new Date().toLocaleDateString() !== card.date,
   )
-const getDoneCards = state => state.doneCards
+
+const getDoneCards = state => state.cards.doneCards
+
+const getIsLoading = state => state.cards.loading
+
+const getError = state => state.cards.error
 
 const cardsSelectors = {
   getActiveTodayCards,
   getActiveTomorrowCards,
   getDoneCards,
+  getIsLoading,
+  getError
 }
 export default cardsSelectors
