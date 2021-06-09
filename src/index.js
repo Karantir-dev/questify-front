d8f6f416de8af7d0b2b4af581819b5dbd052818f
Alina Bovdyr <alinabovdyr92@gmail.com>
2021-06-09 15:22:51 +0300

    Fix auth-redux

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ import App from './App'
 import entireStore from './Redux/store'
 
 import './index.css'
-// import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={entireStore.store}>
-      {/* <PersistGate loading={null} persistor={entireStore.persistor}> */}
+      <PersistGate loading={null} persistor={entireStore.persistor}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-      {/* </PersistGate> */}
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
