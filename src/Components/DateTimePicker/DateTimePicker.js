a67d09b53b706e59e7f5d9feb58bd7038af628ce
Viktoriia Strelnikova <v.strelnikova91@gmail.com>
2021-06-13 18:28:28 +0300

    fix styles for picker

--- src/Components/DateTimePicker/DateTimePicker.js
+++ src/Components/DateTimePicker/DateTimePicker.js
@@ -16,18 +16,19 @@ class DateTimePicker extends Component   {
   render() {
     const { date } = this.state;
     return (
-      <div className="flatpickr">
-        <Flatpickr options={{ minDate: date, wrap: true }}
-        data-enable-time
-        value={date}
-        onChange={date => {
+      <div className="date-time-container">
+        <Flatpickr
+          data-enable-time
+          value={date}
+          minDate={date}
+          onChange={date => {
           this.setState({ date });
         }}
         />
         <Icon
           title="toggle"
           data-toggle
-          className="flatpickrInputIconCalendar input-button"
+          className="flatpickrInputIconCalendar"
           name="calendar"
           color="#00d7ff"
           size={14}
