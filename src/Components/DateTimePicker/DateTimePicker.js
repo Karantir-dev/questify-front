f2e46ba1572bea8cb5fbe7d2a0b16a509ced6918
Vitalij <almizvitalij@gmail.com>
2021-06-13 12:36:21 +0300

    fix main Page styles

--- src/Components/DateTimePicker/DateTimePicker.js
+++ src/Components/DateTimePicker/DateTimePicker.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import DateTimePickerAPI from 'react-datetime-picker';
-import './DateTimePicker.module.css';
+import React from 'react'
+import DateTimePickerAPI from 'react-datetime-picker'
+import './DateTimePicker.css'
 
 function DateTimePicker({ date, handleDateChange }) {
   const minDate = new Date()
-    
+
   return (
     <div className="date-time-picker-container">
       <DateTimePickerAPI
-        onChange={(date => {
+        onChange={date => {
           handleDateChange(date.getTime())
-        })}
+        }}
         value={new Date(date) || new Date()}
         minDate={minDate}
       />
