f843e8470a0c3f3c0d3c747808138154f0d437f0
Smekalin Sergei <crypto.mnr@gmail.com>
2021-06-14 13:59:21 +0300

    Working pagination
    Need some style improvements

--- src/Components/CardList/CardList.js
+++ src/Components/CardList/CardList.js
@@ -1,47 +1,76 @@
+import { useState } from 'react'
 import PropTypes from 'prop-types'
+import ReactPaginate from 'react-paginate'
 import Card from '../Card/Card'
 import CardInfo from '../../Components/CardInfo/CardInfo'
 import CreateEditCard from '../CreateEditCard/CreateEditCard'
 
 import s from './CardList.module.css'
+import './Pagination.css'
 
 function CardList({ isCreateFormShown = false, onCloseForm = null, cards }) {
-  return (
-    <ul className={s.cardList}>
-      {isCreateFormShown && (
-        <li className={s.cardListItem}>
-          <CreateEditCard handleHideCard={onCloseForm} />
-        </li>
-      )}
+  const [offset, setOffset] = useState(0)
 
-      {/* {
+  const perPage = 5
+  const pageCount = Math.ceil(cards.length / perPage)
+  const slicedCards = cards.slice(offset, offset + perPage)
+
+  const handlePageClick = e => {
+    const selectedPage = e.selected
+    setOffset(Math.ceil(selectedPage * perPage))
+  }
+
+  return (
+    <>
+      <ul className={s.cardList}>
+        {isCreateFormShown && (
+          <li className={s.cardListItem}>
+            <CreateEditCard handleHideCard={onCloseForm} />
+          </li>
+        )}
+
+        {/* {
         <CardInfo title="To add a new card, click the button in the lower right corner" />
       } */}
 
-      {cards.map(
-        ({
-          id,
-          isChallenge,
-          difficulty,
-          category,
-          deadline,
-          text,
-          isCompleted,
-        }) => (
-          <li className={s.cardListItem} key={id}>
-            <Card
-              id={id}
-              isChallenge={isChallenge}
-              difficulty={difficulty}
-              category={category}
-              deadline={deadline}
-              text={text}
-              isCompleted={isCompleted}
-            />
-          </li>
-        ),
+        {slicedCards.map(
+          ({
+            id,
+            isChallenge,
+            difficulty,
+            category,
+            deadline,
+            text,
+            isCompleted,
+          }) => (
+            <li className={s.cardListItem} key={id}>
+              <Card
+                id={id}
+                isChallenge={isChallenge}
+                difficulty={difficulty}
+                category={category}
+                deadline={deadline}
+                text={text}
+                isCompleted={isCompleted}
+              />
+            </li>
+          ),
+        )}
+      </ul>
+      {cards.length > perPage && (
+        <ReactPaginate
+          pageCount={pageCount}
+          pageRangeDisplayed={0}
+          marginPagesDisplayed={1}
+          previousLabel={'prev'}
+          nextLabel={'next'}
+          breakLabel={null}
+          onPageChange={handlePageClick}
+          containerClassName={'pagination'}
+          activeClassName={'active'}
+        />
       )}
-    </ul>
+    </>
   )
 }
 
